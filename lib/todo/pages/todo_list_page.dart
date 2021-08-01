8cbce7508d0e2ce120c6c24cd83902fb00afec83
Frappuccino001 <fumi00hiro@icloud.com>
2021-08-02 00:02:56 +0900

    やることリスト画面を作成

---
+++ lib/todo/pages/todo_list_page.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+import '../models/todo.dart';
+
+class TodoListPage extends StatefulWidget {
+  @override
+  _TodoListPageState createState() => _TodoListPageState();
+}
+
+class _TodoListPageState extends State<TodoListPage> {
+  List<Todo> _todoItems = [
+    Todo('牛乳を買う', Icons.description),
+    Todo('物件探す', Icons.local_grocery_store),
+  ];
+
+  void _addTodo(Todo todo) {
+    setState(() => _todoItems.add(todo));
+  }
+
+  void _deleteTodo(int index) {
+    setState(() => _todoItems.removeAt(index));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('やること'),
+      ),
+      body: ListView.builder(
+        itemCount: _todoItems.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Card(
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(width: 1.0, color: Colors.black),
+              ),
+              child: ListTile(
+                leading: Icon(
+                  _todoItems[index].icon,
+                  size: 35.0,
+                ),
+                title: Text(_todoItems[index].title),
+                trailing: IconButton(
+                  icon: Icon(Icons.more_vert),
+                  onPressed: () => showDialog(
+                    context: context,
+                    builder: (BuildContext context) => AlertDialog(
+                      title: Text(_todoItems[index].title),
+                      actions: [
+                        IconButton(
+                          icon: Icon(Icons.delete),
+                          color: Colors.red,
+                          onPressed: () {
+                            _deleteTodo(index);
+                            Navigator.pop(context);
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          final Todo todo = await Navigator.of(context)
+              .push(MaterialPageRoute(builder: (context) => TodoCreatePage()));
+          _addTodo(todo);
+        },
+        tooltip: 'Add Todo',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
