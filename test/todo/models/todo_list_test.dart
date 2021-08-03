029e0be3c0e7ffe9d113cf1c2a2ee3986d050cce
Frappuccino001 <fumi00hiro@icloud.com>
2021-08-03 23:11:33 +0900

    todo_list_testを作成

---
+++ test/todo/models/todo_list_test.dart
@@ -0,0 +1,50 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
+
+import 'package:daisy/todo/models/todo.dart';
+import 'package:daisy/todo/models/todo_list.dart';
+
+void main() {
+  group('TodoList', () {
+    test('TodoList should be List<Todo>', () {
+      final todoList = TodoList([
+        Todo('test1', Icons.add),
+        Todo('test2', Icons.minimize),
+        Todo('test3', Icons.document_scanner),
+      ]);
+      expect(todoList.todoItems.length, 3);
+    });
+
+    test('TodoList should include Todo', () {
+      final todoList = TodoList([
+        Todo('test1', Icons.add),
+        Todo('test2', Icons.minimize),
+        Todo('test3', Icons.document_scanner),
+      ]);
+      expect(todoList.todoItems[0].title, 'test1');
+      expect(todoList.todoItems[0].icon, Icons.add);
+    });
+
+    test('TodoList can add Todo in list', () {
+      final todoList = TodoList([
+        Todo('test1', Icons.add),
+        Todo('test2', Icons.minimize),
+        Todo('test3', Icons.document_scanner),
+      ]);
+      final addedTodoList = todoList.add(Todo('test4', Icons.face));
+      expect(addedTodoList.todoItems[3].title, 'test4');
+      expect(addedTodoList.todoItems[3].icon, Icons.face);
+    });
+
+    test('TodoList can remove Todo in list', () {
+      final todoList = TodoList([
+        Todo('test1', Icons.add),
+        Todo('test2', Icons.minimize),
+        Todo('test3', Icons.document_scanner),
+      ]);
+      final addedTodoList = todoList.removeAt(0);
+      expect(addedTodoList.todoItems[0].title, 'test2');
+      expect(addedTodoList.todoItems[0].icon, Icons.minimize);
+    });
+  });
+}
