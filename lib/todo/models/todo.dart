9fe913531663eb1bb3455d4b3211d33ca96711a8
Frappuccino001 <fumi00hiro@icloud.com>
2021-08-04 22:11:23 +0900

    deadLineを追加

--- lib/todo/models/todo.dart
+++ lib/todo/models/todo.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class Todo {
   String title;
   IconData icon;
+  DateTime deadLine;
 
-  Todo(this.title, this.icon);
+  Todo(this.title, this.icon, this.deadLine);
 }
