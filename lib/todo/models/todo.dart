d5cd8d3f773fc01fa87e6c5b6c0d03881dd51ca6
Frappuccino001 <fumi00hiro@icloud.com>
2021-08-04 22:21:52 +0900

    deadlineをStringで返すメソッドを作成

--- lib/todo/models/todo.dart
+++ lib/todo/models/todo.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 class Todo {
   String title;
@@ -6,4 +8,11 @@ class Todo {
   DateTime deadLine;
 
   Todo(this.title, this.icon, this.deadLine);
+
+  String toStringDeadline() {
+    initializeDateFormatting('ja');
+    DateFormat format = DateFormat.yMMMd('ja');
+    String date = format.format(deadLine);
+    return date;
+  }
 }
