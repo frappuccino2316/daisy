665996f7d2f386dd01f0c07bb5744f8ad5649853
Frappuccino001 <fumi00hiro@icloud.com>
2021-08-12 21:32:16 +0900

    hiveを設定

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
+import 'todo/models/todo.dart';
 import 'todo/views/todo_list_page.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(TodoAdapter());
+  await Hive.openBox('todo');
   runApp(MyApp());
 }
 
