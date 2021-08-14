827b88d121ba90076156644481706d2253625485
Frappuccino001 <fumi00hiro@icloud.com>
2021-08-14 14:12:12 +0900

    testを修正

--- test/todo/models/todo_test.dart
+++ test/todo/models/todo_test.dart
@@ -5,7 +5,8 @@ import 'package:daisy/todo/models/todo.dart';
 void main() {
   group('Todo', () {
     test('Todo should return String deadline', () {
-      Todo todo = Todo('test1', DateTime.parse('1996-02-15 20:18:04Z'));
+      Todo todo = Todo('test1', 'description\n詳細情報です',
+          DateTime.parse('1996-02-15 20:18:04Z'));
       expect(todo.toStringDeadline(), '1996年2月15日');
     });
   });
