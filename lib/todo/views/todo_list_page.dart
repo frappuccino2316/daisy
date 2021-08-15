e79645063a5d279c9d89b0d9cdb6c6f0eb2eab93
Frappuccino001 <fumi00hiro@icloud.com>
2021-08-16 00:39:10 +0900

    編集できるように修正

--- lib/todo/views/todo_list_page.dart
+++ lib/todo/views/todo_list_page.dart
@@ -3,6 +3,7 @@ import 'package:hive_flutter/hive_flutter.dart';
 
 import '../models/todo.dart';
 import 'create_todo_page.dart';
+import 'edit_todo_page.dart';
 
 class TodoListPage extends StatefulWidget {
   @override
@@ -30,25 +31,22 @@ class _TodoListPageState extends State<TodoListPage> {
                 title: Text(_todoBox.values.toList()[index].title),
                 subtitle:
                     Text(_todoBox.values.toList()[index].toStringDeadline()),
-                trailing: IconButton(
-                  icon: const Icon(Icons.more_vert),
-                  onPressed: () => showDialog(
-                    context: context,
-                    builder: (BuildContext context) => AlertDialog(
-                      title: Text(_todoBox.values.toList()[index].title),
-                      actions: [
-                        IconButton(
-                          icon: const Icon(Icons.delete),
-                          color: Colors.red,
-                          onPressed: () {
-                            _todoBox.deleteAt(index);
-                            setState(() {});
-                            Navigator.pop(context);
-                          },
-                        ),
-                      ],
-                    ),
-                  ),
+                trailing: PopupMenuButton<String>(
+                  onSelected: (String selected) {
+                    popUpMenuSelected(selected, index);
+                  },
+                  itemBuilder: (BuildContext context) {
+                    return <PopupMenuEntry<String>>[
+                      const PopupMenuItem(
+                        child: Text('編集'),
+                        value: '編集',
+                      ),
+                      const PopupMenuItem(
+                        child: Text('削除'),
+                        value: '削除',
+                      ),
+                    ];
+                  },
                 ),
               ),
             ),
@@ -68,5 +66,46 @@ class _TodoListPageState extends State<TodoListPage> {
         child: const Icon(Icons.add),
       ),
     );
+  }
+
+  void popUpMenuSelected(String selected, int index) async {
+    Todo _todoItem = _todoBox.values.toList()[index];
+
+    switch (selected) {
+      case '編集':
+        final Todo? todo = await Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) => EditTodoPage(_todoItem)));
+        if (todo != null) {
+          setState(() {
+            _todoItem.title = todo.title;
+            _todoItem.description = todo.description;
+            _todoItem.deadLine = todo.deadLine;
+            _todoItem.save();
+          });
+        }
+        break;
+
+      case '削除':
+        () => showDialog(
+            context: context,
+            builder: (BuildContext context) => AlertDialog(
+                  title: Text(_todoBox.values.toList()[index].title),
+                  actions: [
+                    IconButton(
+                      icon: const Icon(Icons.delete),
+                      color: Colors.red,
+                      onPressed: () {
+                        _todoBox.deleteAt(index);
+                        setState(() {});
+                        Navigator.pop(context);
+                      },
+                    ),
+                  ],
+                ));
+        break;
+
+      default:
+        break;
+    }
   }
 }
