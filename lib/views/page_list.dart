e1d133260d648f67fd1a29b567b2de36e3ba8515
Frappuccino001 <fumi00hiro@icloud.com>
2021-08-17 23:56:40 +0900

    BottomNavigationBarを導入

---
+++ lib/views/page_list.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+import 'todo_list_page.dart';
+
+class PageList extends StatefulWidget {
+  const PageList({Key? key}) : super(key: key);
+
+  @override
+  _PageListState createState() => _PageListState();
+}
+
+class _PageListState extends State<PageList> {
+  final List<Widget> _pageList = <Widget>[
+    TodoListPage(),
+    const Text('2ページ目'),
+    const Text('3ページ目'),
+    const Text('4ページ目'),
+  ];
+
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: _pageList[_selectedIndex]),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.task),
+            label: 'Task',
+            backgroundColor: Colors.lightBlue,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.memory),
+            label: 'Memo',
+            backgroundColor: Colors.lightBlue,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications),
+            label: 'Notification',
+            backgroundColor: Colors.lightBlue,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Setting',
+            backgroundColor: Colors.lightBlue,
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.white,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
