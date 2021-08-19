b08c88448373ec6e92ed42f823b4a1554f6e35ad
Frappuccino001 <fumi00hiro@icloud.com>
2021-08-19 22:56:07 +0900

    StatefulWidgetに修正

--- lib/views/components/page_app_bar.dart
+++ lib/views/components/page_app_bar.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
 
-class PageAppBar extends AppBar {
+class PageAppBar extends StatefulWidget with PreferredSizeWidget {
   final String _title;
 
   PageAppBar(this._title);
 
   @override
+  _PageAppBarState createState() => _PageAppBarState();
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
+
+class _PageAppBarState extends State<PageAppBar> {
+  @override
   Widget build(BuildContext context) {
     return AppBar(
       title: Text(
-        _title,
+        widget._title,
         style:
             const TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
       ),
