25f06da4948c77bb75f595afdd1bc7d15c513a97
Frappuccino001 <fumi00hiro@icloud.com>
2021-08-23 00:02:32 +0900

    CalendarEventを作成

---
+++ lib/models/calendar/calendar_event.dart
@@ -0,0 +1,51 @@
+import 'package:intl/intl.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+part 'calendar_event.g.dart';
+
+@HiveType(typeId: 1)
+class CalendarEvent extends HiveObject {
+  @HiveField(0)
+  String title;
+
+  @HiveField(1)
+  String detail;
+
+  @HiveField(2)
+  DateTime startDateTime;
+
+  @HiveField(3)
+  DateTime endingDateTime;
+
+  CalendarEvent(
+      this.title, this.detail, this.startDateTime, this.endingDateTime);
+
+  String toDateStartDateTime() {
+    initializeDateFormatting('ja');
+    DateFormat format = DateFormat.yMMMd('ja');
+    String startDate = format.format(startDateTime);
+    return startDate;
+  }
+
+  String toDateEndingDateTime() {
+    initializeDateFormatting('ja');
+    DateFormat format = DateFormat.yMMMd('ja');
+    String endingDate = format.format(endingDateTime);
+    return endingDate;
+  }
+
+  String toHourMinuteStartDateTime() {
+    initializeDateFormatting('ja');
+    DateFormat format = DateFormat.Hm('ja');
+    String startTime = format.format(startDateTime);
+    return startTime;
+  }
+
+  String toHourMinuteEndingDateTime() {
+    initializeDateFormatting('ja');
+    DateFormat format = DateFormat.Hm('ja');
+    String endingTime = format.format(endingDateTime);
+    return endingTime;
+  }
+}
