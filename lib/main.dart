af631b29efb2cbc5987d729d148823024dfdefc2
Frappuccino001 <fumi00hiro@icloud.com>
2021-08-24 00:34:03 +0900

    CalendarEventAdapterを接続

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 import 'models/todo/todo.dart';
+import 'models/calendar/calendar_event.dart';
 import 'views/page_list.dart';
 
 void main() async {
   await Hive.initFlutter();
   Hive.registerAdapter(TodoAdapter());
+  Hive.registerAdapter(CalendarEventAdapter());
   await Hive.openBox('todo');
+  await Hive.openBox('calendar');
   runApp(MyApp());
 }
 
