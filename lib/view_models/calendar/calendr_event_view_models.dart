be2593470ac9c0ef5b41d6766d6fda7a15f828af
Frappuccino001 <fumi00hiro@icloud.com>
2021-08-25 00:38:31 +0900

    <Datetime, List<CalendarEvent>>を返すメソッドを作成

--- lib/view_models/calendar/calendr_event_view_models.dart
+++ lib/view_models/calendar/calendr_event_view_models.dart
@@ -5,8 +5,22 @@ import 'package:daisy/models/calendar/calendar_event.dart';
 class CalendarEventViewModel {
   final _calendarEventBox = Hive.box('calendar_event');
 
-  List<dynamic> getCalendarEvents() {
+  List<dynamic> getAllCalendarEvents() {
     List<dynamic> _calendarEventList = _calendarEventBox.values.toList();
     return _calendarEventList;
+  }
+
+  Map<DateTime, List<CalendarEvent>> getMapOfCalendarEvent() {
+    final List<dynamic> events = getAllCalendarEvents();
+    Map<DateTime, List<CalendarEvent>> map = {};
+
+    for (CalendarEvent event in events) {
+      if (map.containsKey(event.startDateTime)) {
+        map[event.startDateTime]!.add(event);
+      } else {
+        map[event.startDateTime] = [event];
+      }
+    }
+    return map;
   }
 }
