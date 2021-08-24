bd5d017c9b51bd7d6f9540bd49ac3758da2b872f
Frappuccino001 <fumi00hiro@icloud.com>
2021-08-25 00:18:32 +0900

    buttonはいらない

--- lib/views/calendar/calendar_page.dart
+++ lib/views/calendar/calendar_page.dart
@@ -1,9 +1,10 @@
+import 'package:daisy/view_models/calendar/calendr_event_view_models.dart';
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:intl/intl.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
-import '../widgets/page_app_bar.dart';
+import 'package:daisy/views/widgets/page_app_bar.dart';
 
 class CalendarPage extends StatefulWidget {
   @override
@@ -11,6 +12,7 @@ class CalendarPage extends StatefulWidget {
 }
 
 class _CalendarPageState extends State<CalendarPage> {
+  // List<dynamic> events = CalendarEventViewModel().getCalendarEvents();
   CalendarFormat _calendarFormat = CalendarFormat.month;
   DateTime _focusedDay = DateTime.now();
   DateTime? _selectedDay;
@@ -24,6 +26,7 @@ class _CalendarPageState extends State<CalendarPage> {
         firstDay: DateTime.utc(2020, 1, 1),
         lastDay: DateTime.utc(2100, 12, 31),
         focusedDay: _focusedDay,
+        headerStyle: const HeaderStyle(formatButtonVisible: false),
         calendarFormat: _calendarFormat,
         selectedDayPredicate: (day) {
           return isSameDay(_selectedDay, day);
@@ -52,13 +55,13 @@ class _CalendarPageState extends State<CalendarPage> {
             );
           }
         },
-        onFormatChanged: (format) {
-          if (_calendarFormat != format) {
-            setState(() {
-              _calendarFormat = format;
-            });
-          }
-        },
+        // onFormatChanged: (format) {
+        //   if (_calendarFormat != format) {
+        //     setState(() {
+        //       _calendarFormat = format;
+        //     });
+        //   }
+        // },
         onPageChanged: (focusedDay) {
           _focusedDay = focusedDay;
         },
