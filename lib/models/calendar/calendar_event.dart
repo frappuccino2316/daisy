0bfd95a2dde26d25258e24ceebce2ce5a1a41c0e
Frappuccino001 <fumi00hiro@icloud.com>
2021-08-26 21:31:12 +0900

    コメントアウト

--- lib/models/calendar/calendar_event.dart
+++ lib/models/calendar/calendar_event.dart
@@ -1,5 +1,5 @@
-import 'package:intl/intl.dart';
-import 'package:intl/date_symbol_data_local.dart';
+// import 'package:intl/intl.dart';
+// import 'package:intl/date_symbol_data_local.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 part 'calendar_event.g.dart';
