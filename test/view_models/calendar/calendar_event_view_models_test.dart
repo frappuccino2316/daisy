95e96d16856d5da4a04784977b61026f056b113e
Frappuccino001 <fumi00hiro@icloud.com>
2021-08-31 23:57:46 +0900

    updateのテストを作成

--- test/view_models/calendar/calendar_event_view_models_test.dart
+++ test/view_models/calendar/calendar_event_view_models_test.dart
@@ -8,22 +8,46 @@ import 'package:daisy/view_models/calendar/calendar_event_view_models.dart';
 
 void main() async {
   await initialiseHive();
-  await Hive.openBox('calendar_event');
-  group('Test CalendarEventViewModel', () {
-    CalendarEventViewModel viewModel = CalendarEventViewModel();
 
+  group('Test CalendarEventViewModel', () {
     tearDown(() async {
       // 最後に必ずクリア
       await Hive.deleteFromDisk();
     });
 
-    test('CalendarEventViewModel can write in hive', () {
+    test('CalendarEventViewModel can create in hive', () async {
+      await Hive.openBox('calendar_event');
+      CalendarEventViewModel _viewModel = CalendarEventViewModel();
+
       CalendarEvent _calendarEvent = CalendarEvent('test1', 'detail test 1',
           DateTime.utc(1996, 2, 15), DateTime.utc(1999, 8, 22));
-      viewModel.addCalendarEvent(_calendarEvent);
+      _viewModel.addCalendarEvent(_calendarEvent);
 
-      List _calendarEventList = viewModel.getAllCalendarEvents();
+      List _calendarEventList = _viewModel.getAllCalendarEvents();
       expect(_calendarEventList, [_calendarEvent]);
+    });
+
+    test('CalendarEventViewModel can update in hive', () async {
+      await Hive.openBox('calendar_event');
+      CalendarEventViewModel _viewModel = CalendarEventViewModel();
+
+      CalendarEvent _calendarEventFirst = CalendarEvent(
+          'test1',
+          'detail test 1',
+          DateTime.utc(1996, 2, 15),
+          DateTime.utc(1999, 8, 22));
+      _viewModel.addCalendarEvent(_calendarEventFirst);
+
+      List _calendarEventList = _viewModel.getAllCalendarEvents();
+
+      CalendarEvent _calendarEventSecond = CalendarEvent(
+          'test2',
+          'detail test 2',
+          DateTime.utc(1993, 8, 22),
+          DateTime.utc(1994, 3, 13));
+      _calendarEventList[0] = _calendarEventSecond;
+
+      expect(_calendarEventList, [_calendarEventSecond]);
     });
   });
 }
