c676925ca9e2b5c55c0c4f735815846ea318fad9
Frappuccino001 <fumi00hiro@icloud.com>
2021-09-05 23:12:28 +0900

    通知画面はいらない

--- lib/views/page_list.dart
+++ lib/views/page_list.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 import 'todo/todo_list_page.dart';
 import 'calendar/calendar_page.dart';
-import 'notification/notification_page.dart';
 import 'setting/setting_page.dart';
 
 class PageList extends StatefulWidget {
@@ -16,7 +15,6 @@ class _PageListState extends State<PageList> {
   final List<Widget> _pageList = <Widget>[
     TodoListPage(),
     CalendarPage(),
-    NotificationPage(),
     SettingPage(),
   ];
 
